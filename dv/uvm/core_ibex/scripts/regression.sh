dfe7f67a22cb4a341edb0ead2d2e487e629b1b2f
Austin Ramirez <a2zaustin@gmail.com>
2025-11-13 19:44:46 -0600

    updating log path to be in out/ folder

--- dv/uvm/core_ibex/scripts/regression.sh
+++ dv/uvm/core_ibex/scripts/regression.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=regression          # Job name
-#SBATCH --mail-type=ALL                # Mail Events (NONE,BEGIN,FAIL,END,ALL)
-#SBATCH --mail-user=a2zaustin@tamu.edu # Replace with your email address
-#SBATCH --ntasks=8                     # Run on 8 Cores
-#SBATCH --time=72:00:00                # Time limit hh:mm:ss
-#SBATCH --output=regression%j.log      # Standard output and error log
-#SBATCH --qos=olympus-cpu-research     # Do not change
-#SBATCH --partition=cpu-research       # Do not change
+#SBATCH --job-name=regression                     # Job name
+#SBATCH --mail-type=ALL                           # Mail Events (NONE,BEGIN,FAIL,END,ALL)
+#SBATCH --mail-user=a2zaustin@tamu.edu            # Replace with your email address
+#SBATCH --ntasks=8                                # Run on 8 Cores
+#SBATCH --time=72:00:00                           # Time limit hh:mm:ss
+#SBATCH --output=../out/run/regression%j.log      # Standard output and error log
+#SBATCH --qos=olympus-cpu-research                # Do not change
+#SBATCH --partition=cpu-research                  # Do not change
 
 source ~/lowRISC/ibex/env_setup.bash
 
